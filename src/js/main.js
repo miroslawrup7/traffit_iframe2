e0482d7cf2db50641c44730cd604f921b4801006
Mirek Rup <miroslawrup7@gmail.com>
2023-11-02 16:18:08 +0100

    test

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,8 @@ const iframe = document.querySelector("#iframe");
 
 window.addEventListener('message', function(event) {
     if (event.origin === 'https://miroslawrup7.github.io') {
-        iframe.height = event.data + "px";
-        console.log(iframe.height)
+        iframe.height = event.data;
+        // console.log(iframe.height)
     }
 
 });
