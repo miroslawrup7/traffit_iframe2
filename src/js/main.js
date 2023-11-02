d4c5ee61bfca74d8c3196923ed92d9f06d4c6939
Mirek Rup <miroslawrup7@gmail.com>
2023-11-02 15:30:46 +0100

    tes77

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 const iframe = document.querySelector("#iframe");
 
 window.addEventListener('message', function(event) {
-    if (event.origin === 'https://miroslawrup7.github.io') {
+    if (event.origin === 'https://miroslawrup7.github.io/') {
         iframe.height = event.data;
         
     }
