bc1936143864b66ef62233a7e4cf9c99feb73637
Mirek Rup <miroslawrup7@gmail.com>
2023-11-02 16:50:51 +0100

    test

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,9 @@
 const iframe = document.querySelector("#iframe");
 
-let iframeHeight
-
-const test = () => {
-    iframe.height = iframeHeight
-}
-
 window.addEventListener('message', (event) => {
     if (event.origin !== 'https://miroslawrup7.github.io') return
-        iframeHeight = event.data
-        setTimeout(test, 3000);
-        // console.log(iframe.height)
+        iframe.height = event.data;
+        console.log(iframe.height)
     
 
 });
