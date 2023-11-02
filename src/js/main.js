dfcfd4b2fc89934e35513ea2268daca45d7b6daa
Mirek Rup <miroslawrup7@gmail.com>
2023-11-02 14:58:44 +0100

    test

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ const iframe = document.querySelector("#iframe");
 window.addEventListener('message', function(event) {
     if (event.origin === 'https://miroslawrup7.github.io/traffit7_5/dist/') {
         iframe.height = event.data;
+        console.log(iframe.height)
     }
 });
 
